254144ee81b03c481cb89182b7febdb926d4f335
Jesse Kantor <kantorjesse@gmail.com>
2025-07-17 15:03:24 -0400

    Initial commit

---
+++ src/main/java/net/kereeye/breathrock/BreathrockClient.java
@@ -0,0 +1,10 @@
+package net.kereeye.breathrock;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BreathrockClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/kereeye/breathrock/BreathrockDataGenerator.java
@@ -0,0 +1,11 @@
+package net.kereeye.breathrock;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BreathrockDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
