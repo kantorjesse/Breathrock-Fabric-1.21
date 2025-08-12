158ea8b9a6383f1e39afa1169d19aa925ad7e9a7
Jesse Kantor <kantorjesse@gmail.com>
2025-08-11 20:10:09 -0400

    Removed Breathrock Grape and added Breathrock Cucumber, Guava, Mango, and Mint

--- src/main/java/net/kereeye/breathrock/Items/ModItems.java
+++ src/main/java/net/kereeye/breathrock/Items/ModItems.java
@@ -9,8 +9,11 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
-    public static final Item BREATHROCK_GRAPE = registerItem("breathrock_grape", new Item(new Item.Settings()));
     public static final Item BREATHROCK_CLEAR = registerItem("breathrock_clear", new Item(new Item.Settings()));
+    public static final Item BREATHROCK_CUCUMBER = registerItem("breathrock_cucumber", new Item(new Item.Settings()));
+    public static final Item BREATHROCK_GUAVA = registerItem("breathrock_guava", new Item(new Item.Settings()));
+    public static final Item BREATHROCK_MANGO = registerItem("breathrock_mango", new Item(new Item.Settings()));
+    public static final Item BREATHROCK_MINT = registerItem("breathrock_mint", new Item(new Item.Settings()));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(Breathrock.MOD_ID, name), item);
@@ -19,8 +22,11 @@ public class ModItems {
         Breathrock.LOGGER.info("Registering Mod Items for " + Breathrock.MOD_ID);
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
-            entries.add(BREATHROCK_GRAPE);
             entries.add(BREATHROCK_CLEAR);
+            entries.add(BREATHROCK_CUCUMBER);
+            entries.add(BREATHROCK_GUAVA);
+            entries.add(BREATHROCK_MANGO);
+            entries.add(BREATHROCK_MINT);
         });
     }
 }
